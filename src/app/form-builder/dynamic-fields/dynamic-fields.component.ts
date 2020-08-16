d2d9cdcad4a0879b8f30d9b425f921496ee92d7d
Mu'men Tayyem <mumen_tayyem@outlook.com>
2020-08-16 22:23:33 +0400

    fixing other controls

--- src/app/form-builder/dynamic-fields/dynamic-fields.component.ts
+++ src/app/form-builder/dynamic-fields/dynamic-fields.component.ts
@@ -1,30 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { DynamicField, ControlData } from '../models/controlData.model';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dynamic-fields',
   templateUrl: './dynamic-fields.component.html',
   styleUrls: ['./dynamic-fields.component.scss']
 })
-export class DynamicFieldsComponent implements OnInit {
+export class DynamicFieldsComponent implements OnInit, OnDestroy {
+  
 
   @Input() controlData: ControlData;
+  subs: Subscription[] = [];
+
+  ngOnDestroy(): void {
+    this.subs.forEach(sub=>sub.unsubscribe());
+  }
 
   ngOnInit(): void {
-    console.log('hi');
-    if (this.controlData && this.controlData.dynamicFieldsChanged) {
-      this.controlData.dynamicFieldsChanged.subscribe(() => {
-        for (let index = 0; index < this.controlData.dynamicFields.length; index++) {
-          const df = this.controlData.dynamicFields[index];
-          if (!df.form) {
-            df.form = new FormGroup({
-              [df.name]: new FormControl('', [Validators.required]),
-              errorMessage: new FormControl('', [Validators.required])
-            });
-          }
+    this.subs.push(this.controlData.dynamicFieldsChanged.subscribe(() => {
+      for (let index = 0; index < this.controlData.dynamicFields.length; index++) {
+        const df = this.controlData.dynamicFields[index];
+        if (!df.form) {
+          df.form = new FormGroup({
+            [df.name]: new FormControl('', [Validators.required]),
+            errorMessage: new FormControl('', [Validators.required])
+          });
         }
-      });
-    }
+      }
+    }));
   }
 }
